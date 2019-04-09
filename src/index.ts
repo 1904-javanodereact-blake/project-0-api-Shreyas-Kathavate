1782722ce4acd8e7fdfc6bd267641de090e3cffd
shreyas-kathavate <shreyaskathavate010@gmail.com>
2019-04-09 11:55:27 -0400

    new

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { userRouter } from './routers/user-router';
+import { spaceshipRouter } from './routers/spaceship-router';
+import { sessionMiddleware } from './middleware/session.middleware';
+import console = require('console');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`request made with url: ${req.url} and method: ${req.method}`);
+  // const headers = req.rawHeaders;
+  // console.log(headers);
+  next();
+});
+
+// attach an actual object to req.body
+app.use(bodyParser.json()); 
+
+// attach the specific users session data to req.session
+app.use(sessionMiddleware); 
+
+/**
+ * Register Routers
+ */
+app.use('/users', userRouter);
+app.use('/spaceships', spaceshipRouter);
+
+// start up the application
+app.listen(8081, () => {
+  console.log(`application started`);
+});
